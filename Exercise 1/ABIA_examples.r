8c81cfd024d86e15f56f34641145503253760c42
CloudyCieux <39074472+CloudyCieux@users.noreply.github.com>
2020-02-26 10:51:49 -0600

    Class Examples
    Examples of how to approach this first assignment

--- Exercise 1/ABIA_examples.r
+++ Exercise 1/ABIA_examples.r
@@ -1,9 +1,7 @@
-ABIA = read.csv('../data/ABIA.csv')
-
+ABIA = read.csv('ABIA.csv')
 
 names(ABIA)
 dim(ABIA)
-
 
 # focus on carrier delays by day of week (1=Monday)
 delay_summary = ABIA %>%
@@ -11,7 +9,7 @@ delay_summary = ABIA %>%
   group_by(DayOfWeek, UniqueCarrier) %>%
   summarize(n = n(),
             mean_carrier_delay = mean(CarrierDelay), 
-            freq_bad_carrier_delay = sum(CarrierDelay >= 60)/n())
+            freq_bad_carrier_delay = sum(CarrierDelay >= 60) / n())
   
 # quick peak at means for AA
 delay_summary %>%
@@ -26,10 +24,10 @@ delay_summary %>%
 # barplot across carriers
 p0 = ggplot(delay_summary)
 
-p0  + geom_col(aes(x=DayOfWeek, y = freq_bad_carrier_delay)) + 
+p0  + geom_col(aes(x = DayOfWeek, y = freq_bad_carrier_delay)) + 
   facet_wrap(~UniqueCarrier)
 
-p0  + geom_col(aes(x=DayOfWeek, y = mean_carrier_delay)) + 
+p0  + geom_col(aes(x = DayOfWeek, y = mean_carrier_delay)) + 
   facet_wrap(~UniqueCarrier)
   
 # how to order by size of carrier?
@@ -37,18 +35,19 @@ p0  + geom_col(aes(x=DayOfWeek, y = mean_carrier_delay)) +
 Carrier_Total = ABIA %>%
   filter(!is.na(CarrierDelay)) %>% 
   group_by(UniqueCarrier) %>%
-  summarize(n=n()) %>%
+  summarize(n = n()) %>%
   arrange(desc(n))
 
 # define a new factor with levels re-ordered
-delay_summary$UniqueCarrierSort = factor(delay_summary$UniqueCarrier, levels=Carrier_Total$UniqueCarrier)
+delay_summary$UniqueCarrierSort = factor(delay_summary$UniqueCarrier,
+                                         levels = Carrier_Total$UniqueCarrier)
 
 # barplot across carriers, now ordereded!
 p1 = ggplot(delay_summary)
 
 # Mesa Airlines is an interesting story (code YV)
-p1  + geom_col(aes(x=DayOfWeek, y = freq_bad_carrier_delay)) + 
+p1 + geom_col(aes(x = DayOfWeek, y = freq_bad_carrier_delay)) + 
   facet_wrap(~UniqueCarrierSort)
 
-p1  + geom_col(aes(x=DayOfWeek, y = mean_carrier_delay)) + 
+p1 + geom_col(aes(x = DayOfWeek, y = mean_carrier_delay)) + 
   facet_wrap(~UniqueCarrierSort)
--- Exercise 1/green_age.r
+++ Exercise 1/green_age.r
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-greenbuildings = read.csv('../data/greenbuildings.csv')
+greenbuildings = read.csv('greenbuildings.csv')
 
 # Step 1: identify possible confounders
 # let's focus on age
@@ -8,14 +8,14 @@ greenbuildings = read.csv('../data/greenbuildings.csv')
 # Step 2: check whether age is a plausible confounder.
 # First, does age predict rent?
 ggplot(greenbuildings) + 
-  geom_point(aes(x=age, y=Rent))
+  geom_point(aes(x = age, y = Rent))
 
 # yes, about 5 cents per year less in rent
-lm(Rent ~ age, data= greenbuildings)
+lm(Rent ~ age, data = greenbuildings)
 
 # Next, are the ages different b/t green and non-green?
 ggplot(greenbuildings) + 
-  geom_histogram(aes(x=age, y=stat(density)), binwidth=2) + 
+  geom_histogram(aes(x = age, y = stat(density)), binwidth = 2) + 
   facet_grid(green_rating~.)
 
 # Yes!  
@@ -29,7 +29,8 @@ ggplot(greenbuildings) +
 # easy way:
 # define some age groupings
 greenbuildings = mutate(greenbuildings, 
-                        agecat= cut(age, c(0, 10, 20, 30, 50, 75, 200), include.lowest =TRUE))
+                        agecat = cut(age, c(0, 10, 20, 30, 50, 75, 200),
+                                     include.lowest = TRUE))
 
 # now compare rent within age groupings
 summ1 = greenbuildings %>%
@@ -42,8 +43,8 @@ summ1 %>% select(agecat, green_rating, mean_rent) %>%
 
 # in bar plot form
 ggplot(summ1) + 
-  geom_col(aes(x=agecat, y=mean_rent, fill=factor(green_rating)), position='dodge')
+  geom_col(aes(x = agecat, y = mean_rent, fill = factor(green_rating)), position = 'dodge')
 
 # Note: comparison is much easier within categories this way, versus stacked bars!
 ggplot(summ1) + 
-  geom_col(aes(x=agecat, y=mean_rent, fill=factor(green_rating)), position='stack')
+  geom_col(aes(x = agecat, y = mean_rent, fill = factor(green_rating)), position = 'stack')
